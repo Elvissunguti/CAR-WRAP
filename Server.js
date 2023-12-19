0e92d2aeed890340c4003c864f1332f75f77f15e
Elvis Sunguti <elviskendrick99@gmail.com>
2023-12-19 11:11:39 +0300

    Added Procfile for Heroku deployment

--- Server.js
+++ Server.js
@@ -8,16 +8,19 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(
-    'mongodb://carwrap:carwrap@ac-bludfzz-shard-00-00.ncbvigm.mongodb.net:27017,ac-bludfzz-shard-00-01.ncbvigm.mongodb.net:27017,ac-bludfzz-shard-00-02.ncbvigm.mongodb.net:27017/carwrap?ssl=true&replicaSet=atlas-13139h-shard-0&authSource=admin&retryWrites=true&w=majority'
-).then(() => {
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://carwrap:carwrap@ac-bludfzz-shard-00-00.ncbvigm.mongodb.net:27017,ac-bludfzz-shard-00-01.ncbvigm.mongodb.net:27017,ac-bludfzz-shard-00-02.ncbvigm.mongodb.net:27017/carwrap?ssl=true&replicaSet=atlas-13139h-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
     console.log('Connected to MongoDb Atlas!');
 }).catch((err) => {
-    console.log('Error connecting to MongoDB Atlas:', err)
+    console.log('Error connecting to MongoDB Atlas:', err);
 });
 
 app.use("/carwrap", CarDetailsRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
-})
+});
